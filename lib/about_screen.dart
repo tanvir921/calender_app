100f8528c1a78e10130f9acb3b402264a9a6adf1
Tanvir Ahmed <tanvirahmedbinahali@gmail.com>
2024-06-16 10:04:43 +0600

    final commit

--- lib/about_screen.dart
+++ lib/about_screen.dart
@@ -12,23 +12,58 @@ class AboutScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/bg.png',
-            height: 200,
-            width: 200,
-          ),
-          SizedBox(height: 10),
-          Text(
-            Constants.appName,
-            style: TextStyle(fontSize: 30),
-          ),
-          SizedBox(height: 5),
-          Text('Version ${Constants.appVersion}',
-              style: TextStyle(fontSize: 15)),
-        ],
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/bg.png',
+              height: 200,
+              width: 200,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              Constants.appName,
+              style: TextStyle(fontSize: 30),
+            ),
+            const SizedBox(height: 5),
+            const Text('Version ${Constants.appVersion}',
+                style: TextStyle(fontSize: 15)),
+            const SizedBox(height: 10),
+            Container(
+              width: 350,
+              child: const Text(
+                'هذا البرنامج لجميع موظفي وزاره الكهرباء والماء والطاقة المتجددة حيث يمكن متابعة عمل نظام النوبات والصباحي مع خاصية اضافة ايام الاجازات الدوريه والمرضية والطارئة . ',
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text('Created by:'),
+            const Text(
+              'Eng. Marzouq Alshaaban',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const Text('Email: mtsbinshaaban@mew.gov.kw'),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              width: 400,
+              child: Center(
+                child: Text(
+                  'Dedicated to all colleagues Workers in the Ministry of Electricity and Water',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       )),
     );
   }
